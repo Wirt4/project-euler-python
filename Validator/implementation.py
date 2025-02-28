f7a4ea7092c0d94b7fd2c505bc4e4b5e944756e4
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-02-27 20:53:46 -0800

    green

--- Validator/implementation.py
+++ Validator/implementation.py
@@ -11,6 +11,18 @@ class Validator:
         if evaluation:
             return None
         raise PreconditionException(message)
+    
+    def postcondition(self, evaluation = None, message = "PostCondition not met"):
+        """
+        the evalution is a boolean and it is mandatory
+        the message is a string and it is optional
+
+        If the evaluation is False, the function will raise an exception with the message
+        if the value of evaluation is True, the function will return None
+        """
+        if evaluation:
+            return None
+        raise PreconditionException(message)
 
 class PreconditionException(Exception):
     def __init__(self, message="Precondition not met"):
--- Validator/test.py
+++ Validator/test.py
@@ -11,5 +11,14 @@ class Validator(unittest.TestCase):
         validator = implementation.Validator()
         validator.precondition(True)
 
+    def test_postcondition_throws_if_no_input(self):
+        validator = implementation.Validator()
+        with self.assertRaises(Exception):
+            validator.postcondition()
+
+    def test_postcondition_does_not_throw_if_true(self):
+        validator = implementation.Validator()
+        validator.postcondition(True)
+
 if __name__ == '__main__':
     unittest.main()
